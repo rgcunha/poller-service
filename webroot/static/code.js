16ab70ba273f0b01e7075746400712bca94824ab
Ruben Cunha <ruben.cunha@ada.com>
2020-09-20 04:21:21 +0200

    refactor: redesign ui

--- webroot/static/code.js
+++ webroot/static/code.js
@@ -1,24 +1,64 @@
 const listContainer = document.querySelector('#service-list');
-let servicesRequest = new Request('/service');
+const servicesRequest = new Request('/service');
 fetch(servicesRequest)
-.then(function(response) { return response.json(); })
-.then(function(serviceList) {
-  serviceList.forEach(service => {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(service.name + ': ' + service.status));
-    listContainer.appendChild(li);
+  .then(response => response.json())
+  .then((serviceList) => {
+    serviceList.forEach(service => {
+      const li = document.createElement("li");
+      li.setAttribute("id", service.id);
+
+      const deleteBtn = document.createElement("input");
+      deleteBtn.setAttribute("value", "Delete");
+      deleteBtn.setAttribute("type", "button");
+      deleteBtn.onclick = onDelete;
+
+      const statusIcon = document.createElement("span");
+      statusIcon.setAttribute("class", "fa fa-check status");
+
+      li.appendChild(document.createTextNode(service.name + ': ' + service.status));
+      li.appendChild(statusIcon);
+      li.appendChild(deleteBtn);
+      listContainer.appendChild(li);
+    });
   });
-});
 
 const saveButton = document.querySelector('#post-service');
-saveButton.onclick = evt => {
-    let urlName = document.querySelector('#url-name').value;
-    fetch('/service', {
-    method: 'post',
+saveButton.onclick = onSave;
+
+function onDelete(evt) {
+  const id = evt.target.parentElement.id;
+
+  sendRequest({ method: 'delete', path: `/service/${id}` })
+    .then((res) => {
+      if (res.ok) location.reload()
+    })
+}
+
+function onSave(evt) {
+  evt.preventDefault();
+  const name = document.querySelector('#service-name').value;
+  const url = document.querySelector('#service-url').value;
+  const body = JSON.stringify({ url, name });
+
+  sendRequest({ method: 'post', path: '/service', body })
+    .then((res) => {
+      if (res.ok) { location.reload() }
+      else res.text().then(setError);
+    })
+}
+
+function sendRequest({ method, path, body }) {
+  const fetchOptions = {
+    method,
     headers: {
-    'Accept': 'application/json, text/plain, */*',
-    'Content-Type': 'application/json'
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
     },
-  body: JSON.stringify({url:urlName})
-}).then(res=> location.reload());
+    body,
+  };
+  return fetch(path, fetchOptions);
+}
+
+function setError(text) {
+  document.getElementById('error').innerHTML = text;
 }
